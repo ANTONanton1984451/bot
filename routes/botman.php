e2a9c14ca031da5a03f30f4354dfb82d3fb1bfbb
ANTONanton1984451 <anton.shushp@yandex.ru>
2020-12-14 22:19:13 +0300

    First commit

---
+++ routes/botman.php
@@ -0,0 +1,20 @@
+<?php
+use App\Http\Controllers\BotManController;
+
+$botman = resolve('botman');
+
+$botman->hears('Hi', function ($bot) {
+    $bot->reply('Hello!');
+});
+
+
+$botman->hears('Hello BotMan!', function($bot) {
+    $bot->reply('Hello!');
+    $bot->ask('Whats your name?', function($answer, $bot) {
+        $bot->say('Welcome '.$answer->getText());
+    });
+});
+
+
+
+$botman->hears('Start conversation', BotManController::class.'@startConversation');
